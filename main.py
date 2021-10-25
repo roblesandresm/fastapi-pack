e2a99d656f43a45fb12870ba79f2ba15948e91c1
Andres Robles <roblesandresm1@gmail.com>
2021-10-25 15:33:38 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+# python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FstApi
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = True
 
 @app.get("/")
 def home():
@@ -13,3 +28,8 @@ def users(id: int):
         "username": "crashzedran",
         "email": "crashzedrandev@gmail.com",
     }
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
